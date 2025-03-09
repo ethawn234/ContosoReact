9be3bb09c4dc08857599aa9a9ac597de510e6697
ethawn <ethawn234@gmail.com>
2025-03-09 18:23:41 -0500

    basic GET using client api method

---
+++ src/Pizzas.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+
+import { ContosoPizza } from './types/ContosoPizza';
+import { PizzaDTO, Topping } from './types/data-contracts';
+
+const api = new ContosoPizza({
+  baseUrl: "https://localhost:7030"
+});
+
+function Pizzas() {
+  const [pizzas, setPizzas] = useState<PizzaDTO[]>([])
+
+  async function getData() {
+    try {
+      const response = api.contosoPizzaList();
+      const json = await response;
+      console.log(json);
+      return json.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  useEffect(() => {
+    async function fetchData() {
+        const data = await getData();
+
+        if (data) {
+          setPizzas(data);
+        }
+      }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {
+        pizzas.length === 0 
+        ? <p>No pizzas available.</p>
+        : pizzas.map(p => {
+            const toppings = p.toppings.map(t => {
+                return (
+                    <div key={t.id}>
+                        <p>{t.name}</p>
+                        <p>{t.calories}</p>
+                    </div>
+                )
+            })
+            
+            return <div key={p.id}>
+                {p.name}
+                {p.sauce.name}
+                {toppings}
+            </div>
+          })
+      }
+    </>
+  )
+}
+
+export default Pizzas
