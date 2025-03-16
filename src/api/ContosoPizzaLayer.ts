2a12b7d6a0ce44f029483a9ac23ded3bfd81ff76
ethawn <ethawn234@gmail.com>
2025-03-16 10:56:57 -0500

    add remaing crud wrappers

--- src/api/ContosoPizzaLayer.ts
+++ src/api/ContosoPizzaLayer.ts
@@ -1,15 +1,15 @@
 import { AxiosResponse } from "axios";
-import { PizzaDTO } from "../types/data-contracts";
+import { Pizza, PizzaDTO } from "../types/data-contracts";
 import apiClient from "./config/axiosConfig";
 
-export const getPizzasAdmin = () => {
-    return apiClient.adminList()
-}
+export const getPizzasAdmin = async (): Promise<AxiosResponse<Pizza[]>> => await apiClient.adminList();
 
-export const getPizzas = async (): Promise<AxiosResponse<PizzaDTO[]>> => {
-    return await apiClient.contosoPizzaList();
-}
+export const getPizzas = async (): Promise<AxiosResponse<PizzaDTO[]>> => await apiClient.contosoPizzaList();
 
-export const getPizza = async (id: number) => {
-    return await apiClient.contosoPizzaDetail(id);
-}
+export const getPizza = async (id: number): Promise<AxiosResponse<PizzaDTO>> => await apiClient.contosoPizzaDetail(id);
+
+export const postPizza = async (pizza: PizzaDTO) => await apiClient.contosoPizzaCreate(pizza);
+
+export const deletePizza = async (id: number) => await apiClient.contosoPizzaDelete(id);
+
+// export const  = async (pizza: PizzaDTO) => await apiClient.
