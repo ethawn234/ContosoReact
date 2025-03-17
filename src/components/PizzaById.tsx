a48e3763751da4d581e139679952464361982b9b
ethawn234 <ethawn234@gmail.com>
2025-03-17 16:53:22 -0500

    working on fetch states

--- src/components/PizzaById.tsx
+++ src/components/PizzaById.tsx
@@ -5,9 +5,9 @@ import { useQuery } from "@tanstack/react-query";
 
 function PizzaById(){
 	const [id, setId] = useState<number>(0);
-	const { data, isError, isLoading, refetch, isPending, fetchStatus, isSuccess, error } = useQuery({
+	const { data, isError, refetch, isPending, error } = useQuery({
 		queryKey: ['getById', id],
-		queryFn: () => getPizza(id),
+		queryFn: async ({ signal }) => getPizza(id, signal),
 		enabled: !!id, // prevent fetch on mount; use refetch() to fetch after user submits their input
 		select: data => data.data
 	});
@@ -17,13 +17,13 @@ function PizzaById(){
 	const getColHeaders = <T extends Record<string, any>>(obj: T) => Object.keys(obj) as (keyof T)[];
 	const pizzaKeys = data && getColHeaders(data);
 
-	if (isLoading) {
-		return <span>Loading...</span>
-	  }
+	// if (isLoading) {
+	// 	return <span>Loading...</span>
+	//   }
 	
-	  if (isError) {
-		return <span>Error: {error.message}</span>
-	  }
+	//   if (isError) {
+	// 	return <span>Error: {error.message}</span>
+	//   }
 
 	return (
 		<>
@@ -40,11 +40,13 @@ function PizzaById(){
 				<button onClick={() => refetch()}>Get Pizza</button>
 			</label>
 		</form>
-			{/* {isPending ? <p>Loading...</p> : <p>Get a pizza by id</p> }
-			{isError ? <p>Something went wrong!</p> : null}
-			{data ? <p>{data.name} found!</p> : <p>Pizza not found</p>} */}
+			{isPending ? <span>Loading...</span> : null }
+			<div>
+				{isError ? <span>{`Error: ${error.message}`}</span> : null}
+				{data ? <span>{data.name} Pizza found!</span> : <span>Pizza not found</span>}
+			</div>
 			{	
-				data && (
+				!data ? null : (
 					<table>
 						<thead>
 							<tr>
