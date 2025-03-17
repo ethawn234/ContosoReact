e4dd85f3f2ced8d735c5192a15c32ca1af73aa5b
ethawn <ethawn234@gmail.com>
2025-03-17 07:04:04 -0500

    extract sauce & toppings for  render

--- src/components/PizzaById.tsx
+++ src/components/PizzaById.tsx
@@ -1,76 +1,60 @@
+
 import { useState } from "react";
-import { PizzaDTO } from "../types/data-contracts";
 import { getPizza } from "../api/ContosoPizzaService";
+import { useQuery } from "@tanstack/react-query";
 
 function PizzaById(){
-	const [pizza, setPizza] = useState<PizzaDTO>();
 	const [id, setId] = useState<number>(0);
+	const { data, isError, isLoading, refetch, isPending, fetchStatus } = useQuery({
+		queryKey: ['getById', id],
+		queryFn: () => getPizza(id),
+		enabled: !!id, // prevent fetch on mount; use refetch() to fetch after user submits their input
+		select: data => data.data
+	});
 	
-	const handler = () => {
-		if(pizza){
-			setPizza(undefined);
-		}
-		fetchPizza(id);
-	}
-
-	async function fetchPizza(id: number){
-		if(id){
-			const data = await getPizza(id);
-			
-			if(data.data) {
-				setPizza(data.data);
-			}
-		}
-	}
-
-	const renderPizza = () => {
-		const { name, id } = pizza;
-		const toppings = pizza?.toppings?.map(t => t.name).join(', ');
-		const sauce = pizza?.sauce.name;
-
-		return {
-			id,
-			name,
-			sauce,
-			toppings,
-
-		}
-	}
 	// Helper function to extract keys from a generic type
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const getColHeaders = <T extends Record<string, any>>(obj: T) => Object.keys(obj) as (keyof T)[];
-  const pizzaKeys = pizza ? getColHeaders(pizza) : null;
-  
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const getColHeaders = <T extends Record<string, any>>(obj: T) => Object.keys(obj) as (keyof T)[];
+	const pizzaKeys = data && getColHeaders(data);
+	console.log(isError, isLoading, refetch, isPending, fetchStatus)
 	return (
 		<>
-		<h1>Get Pizza By Id</h1>
+		<h1>Get data By Id</h1>
 		<form onSubmit={e => e.preventDefault()}>
 			<label>
-				Pizza ID
-				<input type="number" value={id} onChange={e => !Number.isNaN(e.target.value) && setId(parseInt(e.target.value))} />
-				<button onClick={handler}>Get Pizza</button>
+				Pizza ID: { ' ' }
+				<input type="number" value={id} onChange={e => {
+					const val = parseInt(e.target.value);
+					if(!isNaN(val)){
+						setId(val)
+					}
+				}} />
+				<button onClick={() => refetch()}>Get Pizza</button>
 			</label>
 		</form>
-			{
-				!pizza ?
-				<p>Pizza not Found!</p>
-				:
-				(
+			{fetchStatus === 'idle' && <p>Get Pizza by ID</p> }	
+			{isLoading && <p>Loading...</p>}
+			{isError && <p>Something went wrong!</p>}
+			{data ? <p>{data.name} found!</p> : <p>No pizza found.</p>}
+			{	
+				data && (
 					<table>
 						<thead>
 							<tr>
-								{ pizzaKeys.map(key => <th key={key}>{key.toString().toUpperCase()}</th>)}
+								{ pizzaKeys?.map(key => <th key={key}>{key.toString().toUpperCase()}</th>)}
 							</tr>
 						</thead>
 						<tbody>
-							{
-								
-							}
-							<tr key={pizza.id}>
+							<tr key={data.id}>
 								{
-									pizzaKeys.map(key => (
-										<td key={key}>{String(renderPizza()[key])}</td>
-									))
+									pizzaKeys?.map(key => {
+										const pizza = Array.isArray(data[key])
+										? data[key].map(prop => prop.name).join(', ')
+										: typeof data[key] === 'object'
+										? data[key]?.name
+										: data[key]
+										return <td key={key}>{String(pizza)}</td>
+									})
 								}
 							</tr>
 						</tbody>
