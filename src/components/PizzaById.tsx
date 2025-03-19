b3c96eba19d15e3808ff78ed3f728727971d7794
ethawn <ethawn234@gmail.com>
2025-03-19 07:14:22 -0500

    working getById on user submit

--- src/components/PizzaById.tsx
+++ src/components/PizzaById.tsx
@@ -5,10 +5,12 @@ import { useQuery } from "@tanstack/react-query";
 
 function PizzaById(){
 	const [id, setId] = useState<number>(0);
-	const { data, isError, refetch, isPending, error } = useQuery({
+	const [input, setInput] = useState<number>(0);
+
+	const { data, isError, isLoading, error } = useQuery({
 		queryKey: ['getById', id],
-		queryFn: async ({ signal }) => getPizza(id, signal),
-		enabled: !!id, // prevent fetch on mount; use refetch() to fetch after user submits their input
+		queryFn: () => getPizza(id),
+		enabled: !!id, // The query will not execute until the id exists
 		select: data => data.data
 	});
 	
@@ -17,30 +19,22 @@ function PizzaById(){
 	const getColHeaders = <T extends Record<string, any>>(obj: T) => Object.keys(obj) as (keyof T)[];
 	const pizzaKeys = data && getColHeaders(data);
 
-	// if (isLoading) {
-	// 	return <span>Loading...</span>
-	//   }
-	
-	//   if (isError) {
-	// 	return <span>Error: {error.message}</span>
-	//   }
-
 	return (
 		<>
 		<h1>Get data By Id</h1>
 		<form onSubmit={e => e.preventDefault()}>
 			<label>
 				Pizza ID: { ' ' }
-				<input type="number" value={id} onChange={e => {
+				<input type="number" value={input} onChange={e => {
 					const val = parseInt(e.target.value);
 					if(!isNaN(val)){
-						setId(val)
+						setInput(val)
 					}
 				}} />
-				<button onClick={() => refetch()}>Get Pizza</button>
+				<button onClick={() => setId(input)}>Get Pizza</button>
 			</label>
 		</form>
-			{isPending ? <span>Loading...</span> : null }
+			{isLoading ? <span>Loading...</span> : null }
 			<div>
 				{isError ? <span>{`Error: ${error.message}`}</span> : null}
 				{data ? <span>{data.name} Pizza found!</span> : <span>Pizza not found</span>}
