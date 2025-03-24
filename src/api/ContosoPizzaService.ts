21bb2e007d88ff7f37964a297f93e2fd8aec026d
ethawn234 <ethawn234@gmail.com>
2025-03-24 17:01:45 -0500

    change create wrapper type to custom create body

--- src/api/ContosoPizzaService.ts
+++ src/api/ContosoPizzaService.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from "axios";
-import { Pizza, PizzaDTO } from "../types/data-contracts";
+import { Pizza, PizzaCreateBody, PizzaDTO } from "../types/data-contracts";
 import apiClient from "./config/axiosConfig";
 
 export const getPizzasAdmin = async (): Promise<AxiosResponse<Pizza[]>> => await apiClient.adminList();
@@ -8,7 +8,7 @@ export const getPizzas = async (): Promise<AxiosResponse<PizzaDTO[]>> => await a
 
 export const getPizza = async (id: number): Promise<AxiosResponse<PizzaDTO>> => await apiClient.contosoPizzaDetail(id);
 
-export const postPizza = async (pizza: PizzaDTO) => {
+export const postPizza = async (pizza: PizzaCreateBody) => {
     const newPizza = await apiClient.contosoPizzaCreate(pizza);
     console.log('newPizza: ', newPizza)
     return newPizza;
@@ -17,4 +17,4 @@ export const postPizza = async (pizza: PizzaDTO) => {
 
 export const deletePizza = async (id: number) => await apiClient.contosoPizzaDelete(id);
 
-// export const  = async (pizza: PizzaDTO) => await apiClient.
+// export const  = async (pizza: PizzaDTO) => await apiClient.;
