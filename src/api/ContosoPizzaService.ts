5d52fc61e2e725a6f91e71e3981731be1df9f5f3
ethawn234 <ethawn234@gmail.com>
2025-03-25 13:59:20 -0500

    cleanup

--- src/api/ContosoPizzaService.ts
+++ src/api/ContosoPizzaService.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from "axios";
-import { Pizza, PizzaCreateBody, PizzaDTO } from "../types/data-contracts";
+import { Pizza, PizzaCreateDTO, PizzaDTO } from "../types/data-contracts";
 import apiClient from "./config/axiosConfig";
 
 export const getPizzasAdmin = async (): Promise<AxiosResponse<Pizza[]>> => await apiClient.adminList();
@@ -8,8 +8,10 @@ export const getPizzas = async (): Promise<AxiosResponse<PizzaDTO[]>> => await a
 
 export const getPizza = async (id: number): Promise<AxiosResponse<PizzaDTO>> => await apiClient.contosoPizzaDetail(id);
 
-export const postPizza = async (pizza: PizzaCreateBody) => await apiClient.contosoPizzaCreate(pizza);
+export const postPizza = async (pizza: PizzaCreateDTO) => await apiClient.contosoPizzaCreate(pizza);
 
 export const deletePizza = async (id: number) => await apiClient.contosoPizzaDelete(id);
 
 // export const  = async (pizza: PizzaDTO) => await apiClient.;
+
+export const getToppings = async () => await apiClient.getToppings();
