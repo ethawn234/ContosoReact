5af9ae7766b5c2c8482cf96cd7997fe8ded04636
ethawn234 <ethawn234@gmail.com>
2025-03-25 13:57:36 -0500

    add Toppings component to render Topping options

---
+++ src/components/Toppings.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getToppings } from "../api/ContosoPizzaService";
+import Table from "./Table";
+
+
+export default function Toppings(){
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['toppings'],
+        queryFn: getToppings,
+        select: data => data.data
+    });
+
+    return (
+        <>
+        <h1>Available Toppings</h1>
+        {
+            isError ? <span>Sorry, something went wrong</span>
+                    : isLoading ? <span>Loading...</span>
+                    : (data?.length === 0) ? <p>No pizzas available.</p>
+                    : data && data?.length > 0 ? <Table data={data} /> : null
+        }
+        </>
+    )
+}
