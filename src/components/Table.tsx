cd4e18b0ac538a544039fbec70dfe0ee8317e4a3
ethawn234 <ethawn234@gmail.com>
2025-04-17 16:49:36 -0500

    working on order table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,46 +5,64 @@ import {
   Sauce,
   Topping,
 } from "../types/data-contracts";
-
-// function mapDTO(pizza): PizzaDTO {
-//   const convertedPizza = {
-//     id: pizza.id,
-//     name: pizza.name,
-//     sauce: allSauces.filter((s) => s.id === pizza.sauceId)[0],
-//     toppings: pizza.toppingIds.map(
-//       (id) => allToppings.filter((t) => id === t.id)[0]
-//     ),
-//   };
-//   console.log("convertedPizza: ", convertedPizza);
-//   return convertedPizza;
+// export interface PizzaDTO {
+//   /** @format int32 */
+//   id?: number;
+//   /**
+//    * @minLength 1
+//    * @maxLength 100
+//    */
+//   name: string;
+//   sauce?: Sauce;
+//   toppings?: Topping[] | null;
 // }
 
+// export type PizzaCreateDTO = {
+//   /** @format int32 */
+//   id: number;
+//   /**
+//    * @minLength 1
+//    * @maxLength 100
+//    */
+//   name: string;
+//   sauceId?: number;
+//   toppingIds: number[]; // <-- Correct type definition
+// };
+const handleToppingsIdList = (toppingIdsList: number[]): Topping[] =>
+  toppingIdsList?.map(
+    (id) => allToppings.filter((topping) => topping.id == id)[0]
+  );
+
+function _createDTOtoPizzaDTO(pizza: PizzaCreateDTO): PizzaDTO[] {
+  return [
+    {
+      id: pizza.id,
+      name: pizza.name,
+      sauce: allSauces.filter((s) => s.id === pizza.sauceId)[0],
+      toppings: handleToppingsIdList(pizza.toppingIds),
+    },
+  ];
+}
 export default function Table<
   T extends PizzaDTO | PizzaCreateDTO | Topping | Sauce,
 >({ data }: { data: T[] }) {
+  // data needs to be reassigned to function return
+  // Current Issue: identify when data is typeof PizzaCreateDTO & map cols to PizzaDTO for <Table />
+  // try object.keys.includes
+  // another way separate col headers into helper, run getColHeaders, check if data has field unique to PizzaCreateDTO, then run mapper and regenerate colHeaders.
+  //
+  if (typeof data === "object" && "sauceId" in data[0]) {
+    data = _createDTOtoPizzaDTO(data);
+  }
+
   // Helper function to extract keys from a generic type
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const getColHeaders = <T extends Record<string, any>>(obj: T) =>
     Object.keys(obj) as (keyof T)[];
   const keys = data && data?.length > 0 ? getColHeaders(data[0]) : [];
 
-  // console.log("keys: ", keys, "data: ", data);
-  // let convertedPizza;
+  console.log("data: ", data);
 
-  // if (
-  //   Array.isArray(data) &&
-  //   typeof data[0] === "object" &&
-  //   Object.prototype.hasOwnProperty.call(data[0], "sauceId")
-  // ) {
-  //   console.log("data[0]: ", data[0]);
-  // }
-
-  const handleToppingsIdList = (toppingIdsList: number[]) =>
-    toppingIdsList.map(
-      (id) => allToppings.filter((topping) => topping.id == id)[0].name
-    );
-
-  // PizzaDTO:
   return (
     <table>
       <thead>
@@ -57,45 +75,30 @@ export default function Table<
       <tbody>
         {data?.map((item: T) => (
           <tr key={item.id}>
-            {keys.map((itemKey) => {
-              console.log("data: ", data);
+            {keys.map((col) => {
+              // const formattedData = (function () {
+              //   switch (true) {
+              //     case Array.isArray(item[col]):
+              //       return item[col].map((item) => item.name).join(", ");
+              //     case typeof item[col] == "object" &&
+              //       item[col] !== null &&
+              //       "name" in item[col]:
+              //       return;
+              //     default:
+              //       break;
+              //   }
+              // })();
 
-              // Rewrite. The reason why all ID fields show Tomato is the logic needs to account for Sauce|Topping vs Pizza.
-              // Check condition on line 71. It should account for (number && sauce.name)
-              const formattedData = (() => {
-                const value = item[itemKey];
-
-                switch (true) {
-                  case "isVegin" in data:
-                  case Array.isArray(value) && typeof value[0] === "number":
-                    console.log(1, value);
-                    return handleToppingsIdList(value);
-
-                  case Array.isArray(value) && typeof value[0] === "string":
-                    console.log(2, value);
-                    return value.map((prop) => prop.name).join(", ");
-
-                  case typeof value === "number":
-                    console.log(3, value);
-
-                    return (
-                      allSauces.find((sauce) => sauce.id === value)?.name ||
-                      "You found the secret sauce!"
-                    );
-
-                  case typeof value === "object" &&
-                    value !== null &&
-                    "name" in value:
-                    console.log(4, value);
-                    return (value as { name: string }).name;
-
-                  default:
-                    return value;
-                }
-              })();
-
+              const formattedData = Array.isArray(item[col])
+                ? item[col].map((prop) => prop.name).join(", ") // handle arrays (toppings)
+                : typeof item[col] == "object" &&
+                    item[col] !== null &&
+                    "name" in item[col]
+                  ? (item[col] as { name: string }).name // handle objs (sauce)
+                  : item[col]; // handle primitive fields
+              console.log("formattedData: ", formattedData);
               return (
-                <td key={`${item.id}-${String(itemKey)}`}>
+                <td key={`${item.id}-${String(col)}`}>
                   {String(formattedData ?? "N/A")}
                 </td>
               ); // The nullish coalescing (??) operator is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
